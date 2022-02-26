25f9e24d8d269be378bcb8e9e0d5d3a8447a5846
yonava <yonavossandreae@gmail.com>
2022-02-26 03:05:20 -0500

    test

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:8080"],
+        origin: "*",
         methods: ['GET', "POST"]
     }
 });
