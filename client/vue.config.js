1e2b2b09aa8903703955d847fb9c80351b945b68
yonava <yonavossandreae@gmail.com>
2022-02-26 13:56:13 -0500

    minor changes

--- client/vue.config.js
+++ client/vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://localhost:3000'
+                target: 'http://localhost:1010'
             }
         }
     }
