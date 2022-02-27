8d1fc681f409ca6b4f425fd6ddf7a0aee4123047
yonava <yonavossandreae@gmail.com>
2022-02-26 19:50:42 -0500

    filter

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -9,6 +9,8 @@
         <br>
         <button @click="joinRoom()">Join Room</button>
         <button @click="createRoom()">Create Room</button>
+        <br><br>
+        <h1>{{ errorMessage }}</h1>
       </div>
     </center>
 
@@ -26,6 +28,7 @@
 <script>
 
 import Room from '../components/Room.vue'
+import bannedWords from '../assets/filteredWords.js'
 
 export default {
   data: () => {
@@ -33,7 +36,8 @@ export default {
       menuDisplay: true,
       username: '',
       roomID: '',
-      host: false
+      host: false,
+      errorMessage: ''
     }
   },
   components: {
@@ -50,17 +54,52 @@ export default {
   },
   methods: {
     createRoom() {
+
+      this.validateUsername();
+
+      if (this.errorMessage) return;
+
       this.roomID = 0;
+
       while (this.roomID < 1000) {
         this.roomID = Math.round(Math.random() * 10000);
       }
+
       this.host = true;
       this.menuDisplay = !this.menuDisplay;
       this.roomID = String(this.roomID);
+
     },
     joinRoom() {
+
+      this.validateUsername();
+
+      if (this.errorMessage) return;
+
       this.host = false;
       this.menuDisplay = !this.menuDisplay;
+    },
+    validateUsername() {
+
+      if (!this.username) 
+        return this.errorMessage = 'Enter A Username';
+
+      if (this.username.length < 4)
+        return this.errorMessage = 'Username Is Too Short'
+
+      for (let i in bannedWords.bannedWords) {
+        if (this.username.toLowerCase().includes(bannedWords.bannedWords[i])) return this.errorMessage = 'Inappropriate Username';
+      }
+
+      let dreamStan = ['innit', 'dream', 'dream smp',
+      'notfound', 'georgenotfound', 'badboyhalo']
+      
+      for (let i in dreamStan) {
+        if (this.username.toLowerCase().includes(dreamStan[i])) return this.errorMessage = 'Get Out You Filthy Dream Stan';
+      }
+      
+
+      return this.errorMessage = '';
     }
   },
   watch: {
